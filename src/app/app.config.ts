57e2c2fd58e1af566b7af66702a8c48178849eaa
Preston Lee <preston.lee@prestonlee.com>
2025-09-04 13:42:33 -0700

    CQL Library area enhancements.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,6 +10,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { MomentModule } from 'ngx-moment';
 import { MarkdownModule } from 'ngx-markdown';
 import { provideToastr } from 'ngx-toastr';
+import { HIGHLIGHT_OPTIONS, HighlightJSOptions } from 'ngx-highlightjs';
 
 import { routes } from './app.routes';
 import { SettingsService } from './settings/settings.service';
@@ -28,6 +29,22 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptorsFromDi()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: <HighlightJSOptions>{
+        coreLibraryLoader: () => import('highlight.js/lib/core'),
+        languages: {
+          json: () => import('highlight.js/lib/languages/json'),
+          // javascript: () => import('highlight.js/lib/languages/javascript'),
+          // typescript: () => import('highlight.js/lib/languages/typescript'),
+          // xml: () => import('highlight.js/lib/languages/xml'),
+          // css: () => import('highlight.js/lib/languages/css'),
+          // sql: () => import('highlight.js/lib/languages/sql'),
+          // bash: () => import('highlight.js/lib/languages/bash'),
+          // yaml: () => import('highlight.js/lib/languages/yaml')
+        }
+      }
+    },
     SettingsService,
     LoaderService,
     LibraryService
